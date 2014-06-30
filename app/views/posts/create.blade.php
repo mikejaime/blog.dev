bb4b793c2a33055584cdbf53fc543af30cbbab57
Mike Jaime <mikejaime@yahoo.com>
2014-06-30 13:40:42 -0500

    setup basic blog entry form with sticky fields

---
+++ app/views/posts/create.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.master')
+
+@section('content')
+<div class="container">
+	<h1>Create Post:</h1>
+	<!-- action is sending to the store method in post controller -->
+	<form action="{{{ action('PostsController@store') }}}" method="POST">
+		<label for="Title">Title:</label>
+		<p>
+		<!-- Input::old is making the field sticky -->
+		<input type="text" id="title" name="title" value="{{{ Input::old('title') }}}">
+		</p>
+		<label for="Body">Body:</label>
+		<p>
+		<textarea id="body" name="body"> {{{ Input::old('body') }}} </textarea>
+		</p>
+		<input type="submit">
+	</form>
+</div>
+@stop
