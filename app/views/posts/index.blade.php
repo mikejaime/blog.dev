bf96fb432f5b5d193928e7853599efbe76bbfe70
Mike Jaime <mikejaime@yahoo.com>
2014-07-01 15:35:31 -0500

    added edit buttons for each entry and expermenting with css

--- app/views/posts/index.blade.php
+++ app/views/posts/index.blade.php
@@ -2,13 +2,21 @@
 
 @section('content')
 	
-	<h2></h2>
 	<div class='container'>
+	<h2>WHAT's ON MIKEY's MIND</h2>
 	@foreach ($posts as $post)
-		<h4> {{ link_to_action('PostsController@show', $post->title, array($post->id))}} </h4>
-		<small>{{{$post->created_at}}}</small><br>
-		<p>	{{{$post->body}}} </p>
+		<div>
+			<span>
+				<h4> {{ link_to_action('PostsController@show', $post->title, array($post->id)) }} </h4>
+				<small>{{{$post->created_at}}}</small><br>
+				<p>	{{{$post->body}}} </p>
+			</span>
+			<span>
+				<a href="{{ action('PostsController@edit', $post->id) }}" class="btn btn-default btn-sm">Edit</a>
+			</span>			
+		</div>
 	@endforeach
+	<p>{{ $posts->links() }}</p>
 	<p>{{ link_to_action('PostsController@create', '::: New Post :::')}}</p>
 	</div>
 
