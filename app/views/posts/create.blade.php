08028454a8034ed19275f76eeb6250583bc0c949
Mike Jaime <mikejaime@yahoo.com>
2014-07-01 14:44:14 -0500

    placed form fields inside of divs

--- app/views/posts/create.blade.php
+++ app/views/posts/create.blade.php
@@ -3,19 +3,21 @@
 @section('content')
 <div class="container">
 	<h1>Create Post:</h1>
-	{{ $errors->first('title', '<span class="help-block">:message</span><br>') }}
-	{{ $errors->first('body', '<span class="help-block">:message</span><br>') }}
 	<!-- action is sending to the store method in post controller -->
 	{{ Form::open(array('action'=>'PostsController@store')) }}
-
+	<div>
 		{{ Form::label('title', 'Title: ') }}
 		{{ Form::text('title') }}
-		<br>
+		{{ $errors->first('title', '<span class="help-block">:message</span><br>') }}
+	</div>
+	<div>
 		{{Form::label('body', 'Body: ')}}
 		{{ Form::textarea('body') }}
-		<br>
+		{{ $errors->first('body', '<span class="help-block">:message</span><br>') }}
+	</div>
+	<div>
 		{{ Form::submit('Submit') }}
-
+	</div>		
 	{{ Form::close() }}
 </div>
 @stop
