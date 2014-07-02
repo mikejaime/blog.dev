4fbfdaf13890072c6531041284957432890e3157
Mike Jaime <mikejaime@yahoo.com>
2014-07-02 17:06:33 -0500

    initial commit - created new users table

---
+++ app/database/migrations/2014_07_02_162736_create_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		//
+		Schema::create('users', function($table)
+        {
+            $table->increments('id');
+            $table->string('email', 200)->unique();
+    		$table->string('password', 100);
+            // $table->string('first_name', 50);
+            // $table->string('middle_name', 50)->nullable();
+            // $table->string('last_name', 50);
+            $table->timestamps();
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+		Schema::drop('users');
+	}
+
+}
