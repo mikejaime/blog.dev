d61d1a38a9bdbe7567e29367aefc529d76ff4666
Mike Jaime <mikejaime@yahoo.com>
2014-07-02 13:03:43 -0500

    added delete button

--- app/views/posts/show.blade.php
+++ app/views/posts/show.blade.php
@@ -4,7 +4,12 @@
 
 <div class='container'>
 	<h3> {{{$post->title}}} </h3>
+	<small>{{{$post->created_at->setTimezone('America/Chicago')->format('l, F jS Y @ h:i A')}}}</small><br>
 	<p>	{{{$post->body}}} </p>
+
+	{{ Form::open(array('action' => array('PostsController@destroy', $post->id), 'method' => 'DELETE')) }}
+		{{ Form::submit('Delete') }}
+	{{ Form::close() }}
 </div>
 
 @stop
