4fae513d334f58e4f1e99b5a261cde6533400e27
mike <mikejaime@yahoo.com>
2014-07-11 15:29:23 +0000

    added setSlugAttribute

--- app/models/Post.php
+++ app/models/Post.php
@@ -1,15 +1,50 @@
 <?php
 
-class Post extends Eloquent {
+class Post extends BaseModel {
 
     // tells us what table we're connecting to
     protected $table = 'posts';
 
+    protected $imgDir = 'img-upload';
+
     // Valdation rules for our model properties
     static public $rules = [
     	// | pipes are seperators
-    	'title' => 'required|max:100'
-    	// 'body' => 'required'
+    	'title' => 'required|max:100',
+    	'body' => 'required'
     ];
+
+    public function user() 
+    {
+    	return $this->belongsTo('User');
+    }
+
+	public function addUploadedImage($image) 
+    {
+    	$systemPath = public_path() . '/' . $this->imgDir . '/';
+
+    	$imageName = $this->id . '-' . $image->getClientOriginalName();
+
+    	$image->move($systemPath, $imageName);
+
+    	$this->img_path = '/' . $this->imgDir . '/' . $imageName;
+    }    
+
+    public function renderBody()
+    {
+        // Convert he post body from markdown to HTML using parsedown.
+        // Sanitize the converted HTML with HTML Purifier.
+        // Return the sanitized result
+        $dirty_html = Parsedown::instance()->parse($this->body);
+        $config = HTMLPurifier_Config::createDefault();
+        $purifier = new HTMLPurifier($config);
+        return $purifier->purify($dirty_html);
+    }
+
+    public function setSlugAttribute($value)
+    {
+        $value = str_replace(' ', '-', trim($this->title));
+        $this->attributes['slug'] = strtolower($value);
+    }
 
 }
